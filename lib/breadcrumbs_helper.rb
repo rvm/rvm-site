180a45f9b76eb1c1533ad21c5ce8488ebbaef9c9
Darcy Laycock <sutto@sutto.net>
2010-07-20 15:04:02 +0800

    Implement a helper to generate breadcrumbs

--- lib/breadcrumbs_helper.rb
+++ lib/breadcrumbs_helper.rb
@@ -1,18 +1,20 @@
 module BreadcrumbsHelper
 
-  def breadcrumbs(path, *names)
+  def breadcrumbs(*names)
+    path = @item.identifier
     names.unshift "Documentation"
     parts = path.split("/")
     html = []
     parts.inject([]) do |acc, current|
       acc << current
       url = "#{acc.join('/')}/"
-      html << %(<a href='#{url}'>#{names.shift}</a>)
+      html << link_to(names.shift, url)
       acc
     end
     html *= "&nbsp;&gt;&nbsp;"
-    %(<div class='breadcrumbs'>#{html}</div>)
+    %(<div class='breadcrumbs'>#{html}<hr /></div>)
   end
 
 end
+include Nanoc3::Helpers::LinkTo
 include BreadcrumbsHelper
