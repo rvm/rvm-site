b449a939ae611e9d0994ef03ca67207cbc016847
Wayne E. Seguin <wayneeseguin@gmail.com>
2011-04-07 21:22:27 -0400

    https, remove uservoice.

--- content/assets/javascripts/site.js
+++ content/assets/javascripts/site.js
@@ -3,28 +3,3 @@ google.load("jquery", "1.4.2");
 
 google.load("jqueryui", "1.8.1");
 
-var uservoiceOptions = {
-  /* required */
-  key: 'rvm',
-  host: 'rvm.uservoice.com',
-  forum: '59385',
-  showTab: true,
-  /* optional */
-  alignment: 'left',
-  background_color:'#f00',
-  text_color: 'white',
-  hover_color: '#06C',
-  lang: 'en'
-};
-
-function _loadUserVoice() {
-  var s = document.createElement('script');
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
-  document.getElementsByTagName('head')[0].appendChild(s);
-}
-
-_loadSuper = window.onload;
-
-window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
-
