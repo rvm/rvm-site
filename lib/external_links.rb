5598fbd70a2e02d27915c933c454e0bc98975437
Richard Michael <rmichael@edgeofthenet.org>
2013-03-26 11:26:57 +0100

    Revert "Prefer a local to class metaprogramming; no explicit self."
    This reverts commit 1ec7f49f54494e94172c5df66993d9939340008d.

--- lib/external_links.rb
+++ lib/external_links.rb
@@ -4,28 +4,31 @@ Nanoc::Extra::Checking::Checks.send(:remove_const, :ExternalLinks)
 module Nanoc::Extra::Checking::Checks
   class ExternalLinks < Nanoc::Extra::Checking::Check
     identifiers :external_links, :elinks
+    @count = 4
+
+    class << self
+      attr_accessor :count
+    end
 
     def run
       hrefs =
         Nanoc::Extra::LinkCollector.new(
-          output_filenames.select { |f|
+          self.output_filenames.select { |f|
             File.extname(f) == ".html"
           }, :external
         ).filenames_per_href
 
       list = hrefs.keys.select { |u| u =~ %r!https?://! }
-      find_invalid(list).each do |l|
+      self.find_invalid(list).each do |l|
         filenames = hrefs[l]
         filenames.map do |f|
-          add_issue("#{l} is broken, fix it", subject: f)
+          self.add_issue("#{l} is broken, fix it", subject: f)
         end
       end
     end
 
     def find_invalid(hrefs)
-      count = 4
-
-      count.times do |i|
+      self.class.count.times do |i|
         hydra = Typhoeus::Hydra.new(max_concurrency: 20)
         hrefs.each do |l|
           hydra.queue(
@@ -43,7 +46,7 @@ module Nanoc::Extra::Checking::Checks
 
         hydra.run
 
-        if i < count
+        if i < self.class.count
           (hrefs.empty?) ? break : sleep(i * 5)
         end
       end
