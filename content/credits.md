2d82f18b92de74f495c87b8581e6ebeea5ead735
Michal Papis <mpapis@gmail.com>
2014-10-03 05:31:46 +0200

    upgrading most of the menus to automated enumeration

--- content/credits.md
+++
@@ -1,9 +0,0 @@
----
-title: RVM Credits (Incomplete)
----
-
-## Credits
-
-- [Contributions](/credits/thanks/)
-- [Donations](/credits/donations/)
-- [Funding](/credits/funding/)
--- lib/code_helper.rb
+++ lib/code_helper.rb
@@ -10,6 +10,33 @@ module CodeHelper
     "<li><code>#{html_escape code}</code> - #{text}</li>"
   end
 
+  def items_for(path)
+    @items.select { |item|
+      Regexp.new("^#{path}.+").match(item.path || item.identifier)
+    }.sort_by{ |item|
+      (item.path || item.identifier)
+    }
+  end
+
+  def link_in_menu(item)
+    name = (item.path || item.identifier).split("/").last
+    name = name.gsub(/-/, " ").gsub(/\w+/) { |w| w.capitalize }
+    link_to(name, item)
+  end
+
+  def find_item(path)
+    @items.find { |item| item.path == path }
+  end
+
+  def menu_for(path, wrapper = nil)
+    content = link_in_menu(find_item(path))
+    content = "<#{wrapper}>#{content}</#{wrapper}>" if wrapper
+    content += "\n"
+    content +=
+    "<ul>#{items_for(path).map { |item| "<li>#{link_in_menu(item)}</li>" }.join("\n")}</ul>"
+    content
+  end
+
 end
 
 include Nanoc3::Helpers::HTMLEscape
